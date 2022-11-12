3c873e6c9f7a0a39b62e1ae9f7c85fea161f3ad4
wandersonpereira <wanderson.coord@gmail.com>
2022-11-12 08:22:40 -0300

    Update convert-env.py

--- env/convert-env.py
+++ env/convert-env.py
@@ -4,10 +4,7 @@ secretFile = """
 apiVersion: v1
 kind: Secret
 metadata:
-  annotations:
-    kubectl.kubernetes.io/last-applied-configuration: { ... }
   name: secrets-{0}
-  resourceVersion: "164619"
   uid: cfee02d6-c137-11e5-8d73-42010af00002
 type: Opaque
 data:""".format(os.getenv('APPLICATION_NAME'))
