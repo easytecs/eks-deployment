27bb99beae907c9a0018476b5c7c861b57acb63e
wandersonpereira <wanderson.coord@gmail.com>
2022-11-12 20:06:30 -0300

    Update convert-env.py

--- STBuild/convert-env.py
+++ STBuild/convert-env.py
@@ -8,8 +8,7 @@ kind: ConfigMap
 metadata:
   name: env-{0}
   namespace: st-service
-data:
-""".format(apcliationNameSplited[1])
+data:""".format(apcliationNameSplited[1])
 
 file_object = open('./deployment/config-map.yaml', 'a')
 file_object.write(secretFile)
@@ -21,6 +20,6 @@ for key, value in enumerate(sys.argv):
     keyAndValue = value.split("=")
     if keyAndValue[0].startswith("ST_") and keyAndValue[1] != "":
         print(keyAndValue[1])
-        file_object.write("\n  {0}: {1}".format(keyAndValue[0].replace("ST_", ""), keyAndValue[1]))
+        file_object.write("\n\t{0}: {1}".format(keyAndValue[0].replace("ST_", ""), keyAndValue[1]))
 
 file_object.close()
