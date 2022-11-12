dd8ec795b92a36385146f97a761c6fafd96d5e51
wandersonpereira <wanderson.coord@gmail.com>
2022-11-12 08:14:14 -0300

    Update convert-env.py

--- env/convert-env.py
+++ env/convert-env.py
@@ -3,7 +3,7 @@ import os
 file_object = open('./secrets.yaml', 'a')
 
 for name, value in os.environ.items():
-    if name.startswith("ST_")
+    if name.startswith("ST_"):
         file_object.write("  {0}: {1}".format(name.replace("ST_", ""), value))
 
 file_object.close()
