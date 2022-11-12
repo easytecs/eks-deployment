5d1463b353bcdafe5f202acf7dce8c4c3210679a
Wanderson Pereira <wanderson.pereira@infracommerce.com.br>
2022-11-12 09:04:18 -0300

    adjust

--- env/convert-env.py
+++ env/convert-env.py
@@ -18,7 +18,7 @@ file_object.write(secretFile)
 
 for name, value in sys.argv:
     keyAndValue = value.split("=")
-    if keyAndValue[0].startswith("ST_") and keyAndValue[1] != 1:
+    if keyAndValue[0].startswith("ST_") and keyAndValue[1] != "1":
         file_object.write("  {0}: {1}".format(keyAndValue[0].replace("ST_", ""), keyAndValue[1]))
 
 file_object.close()
