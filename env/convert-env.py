cd668fd3f421236c6f4478bce6fc28e4099dcd9f
Wanderson Pereira <wanderson.pereira@infracommerce.com.br>
2022-11-12 09:02:26 -0300

    adjust

--- env/convert-env.py
+++ env/convert-env.py
@@ -1,4 +1,8 @@
 import os
+import sys
+ 
+# total arguments
+apcliationNameSplited = keyAndValue = sys.argv[0].split("=")
 
 secretFile = """
 apiVersion: v1
@@ -7,14 +11,14 @@ metadata:
   name: secrets-{0}
   uid: cfee02d6-c137-11e5-8d73-42010af00002
 type: Opaque
-data:""".format(os.getenv('APPLICATION_NAME'))
+data:""".format(apcliationNameSplited[1])
 
 file_object = open('./env/secrets.yaml', 'a')
 file_object.write(secretFile)
 
-for name, value in os.environ.items():
-    print(name)
-    if name.startswith("ST_"):
-        file_object.write("  {0}: {1}".format(name.replace("ST_", ""), value))
+for name, value in sys.argv:
+    keyAndValue = value.split("=")
+    if keyAndValue[0].startswith("ST_") and keyAndValue[1] != 1:
+        file_object.write("  {0}: {1}".format(keyAndValue[0].replace("ST_", ""), keyAndValue[1]))
 
 file_object.close()
