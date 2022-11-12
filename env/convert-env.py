d3f05c814c122a1c68ca684af9f9b01070265bcc
wandersonpereira <wanderson.coord@gmail.com>
2022-11-12 08:19:00 -0300

    Update convert-env.py

--- env/convert-env.py
+++ env/convert-env.py
@@ -1,6 +1,19 @@
 import os
 
+secretFile = """
+apiVersion: v1
+kind: Secret
+metadata:
+  annotations:
+    kubectl.kubernetes.io/last-applied-configuration: { ... }
+  name: secrets-{0}
+  resourceVersion: "164619"
+  uid: cfee02d6-c137-11e5-8d73-42010af00002
+type: Opaque
+data:""".format(os.getEnv('APPLICATION_NAME'))
+
 file_object = open('./secrets.yaml', 'a')
+file_object.write(secretFile)
 
 for name, value in os.environ.items():
     if name.startswith("ST_"):
