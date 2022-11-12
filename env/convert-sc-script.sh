51052b8f32f1a9111b5fe8a3cc14e386ec34cfe0
wandersonpereira <wanderson.coord@gmail.com>
2022-11-12 07:53:40 -0300

    Update convert-sc-script.sh

--- env/convert-sc-script.sh
+++ env/convert-sc-script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python convert-env.py
+python ./convert-env.py
 #for i in $(declare -xp | grep --perl-regexp --only-match '(?<=^declare -x )[^=]+' | grep INPUT_); do echo "  $i: ${!i}" >> ./secrets.yaml ; done
