77f01740f92580b7e51486dd913f3f4cd9aadfcd
Wanderson Pereira <wanderson.pereira@infracommerce.com.br>
2022-11-16 09:19:51 -0300

    Add fluxo de deployment da aplicação web

--- STBuild/create-application-deployment.py
+++ STBuild/create-application-deployment.py
@@ -47,7 +47,6 @@ metadata:
     service.beta.kubernetes.io/aws-load-balancer-type: external
     service.beta.kubernetes.io/aws-load-balancer-nlb-target-type: ip
     service.beta.kubernetes.io/aws-load-balancer-scheme: internet-facing
-    service.beta.kubernetes.io/aws-load-balancer-internal: "false"
 spec:
   ports:
   - port: {1}
