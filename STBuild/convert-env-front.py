c662c9ffc35a5aa2d8cbb6e3e61a04c565b7a656
Wanderson Pereira <wanderson.pereira@infracommerce.com.br>
2022-11-16 16:34:45 -0300

    Add fluxo de deployment da aplicação web

--- STBuild/convert-env-front.py
+++ STBuild/convert-env-front.py
@@ -1,16 +1,13 @@
 import sys
  
-apcliationNameSplited =  sys.argv[1].split("=")
-serviceNameSplited =  sys.argv[2].split("=")
-
 secretFile = "CI=false"
 file_object = open('./deployment/.env.production.local', 'a')
 file_object.write(secretFile)
 
 for key, value in enumerate(sys.argv):
-    if key <= 2:
+    if key < 1:
       continue
-
+    
     file_object.write('\n{0}'.format(value))
 
 file_object.close()
