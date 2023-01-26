6cea8d0ed4f101d92fe0f6f48c22060b0ffd567a
Wanderson Pereira <wanderson.coord@gmail.com>
2023-01-26 12:07:29 -0300

    Update env to work with json

---
+++ STBuild/convert-env-front-v2.py
@@ -0,0 +1,14 @@
+import sys
+import json
+ 
+secretFile = "CI=false"
+file_object = open('./deployment/.env.production.local', 'a')
+file_object.write(secretFile)
+
+envJson = sys.argv[3].split("=")
+env = json.loads(envJson[1])
+
+for key, val in env.items():
+  file_object.write('\n{0}={1}'.format(key, val))
+
+file_object.close()
