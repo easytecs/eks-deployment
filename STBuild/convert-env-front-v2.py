c64d17acdbd7cb38fea0bbe79e74348cd3b8e880
wandersonpereira <wanderson.coord@gmail.com>
2023-01-26 17:08:53 -0300

    Update convert-env-front-v2.py

--- STBuild/convert-env-front-v2.py
+++ STBuild/convert-env-front-v2.py
@@ -6,10 +6,7 @@ file_object = open('./deployment/.env.production.local', 'a')
 file_object.write(secretFile)
 
 envJson = sys.argv[1].split("=")
-print("here 1", envJson)
 env = json.loads(envJson[1])
-print"here 2", (env)
-
 
 for key, val in env.items():
   file_object.write('\n{0}={1}'.format(key, val))
