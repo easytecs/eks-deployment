de8990e69433d0482a8b56a8da20eecfdcba5bc4
Wanderson Pereira <wanderson.coord@gmail.com>
2023-01-26 08:55:09 -0300

    Update env to work with json

---
+++ STBuild/convert-env-v2.py
@@ -0,0 +1,27 @@
+import sys
+import re 
+import json
+ 
+apcliationNameSplited =  sys.argv[1].split("=")
+serviceNameSplited =  sys.argv[2].split("=")
+
+secretFile = """
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: env-{0}
+  namespace: {1}
+data:
+  IS_ST: "YES"
+""".format(apcliationNameSplited[1], serviceNameSplited[1])
+
+file_object = open('./deployment/config-map.yaml', 'a')
+file_object.write(secretFile)
+
+envJson = sys.argv[3].split("=")
+env = json.loads(envJson[1])
+
+for key, val in env.items():
+  file_object.write('\n  {0}: "{1}"'.format(key, val))
+
+file_object.close()
